ac14f48012a2c007a6051bff623c7ae867326aab
Daniel van den Berg <idrini@gmail.com>
2014-01-21 17:12:47 +0100

    Initial commit

---
+++ MyALDAList.java
@@ -0,0 +1,61 @@
+import java.util.Iterator;
+
+/**
+ * Created by Happyjuiced on 2014-01-21.
+ */
+public class MyALDAList<T> implements ALDAList<String> {
+    @Override
+    public void add(String element) {
+
+    }
+
+    @Override
+    public void add(int index, String element) {
+
+    }
+
+    @Override
+    public String remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(String element) {
+        return false;
+    }
+
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(String element) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(String element) {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * Returns an iterator over a set of elements of type T.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<String> iterator() {
+        return null;
+    }
+}
